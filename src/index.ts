2aa551cc39f6ca3cd1270b98e520e50ab9467328
Adam Fipke <adamfipke@gmail.com>
2025-08-26 16:08:04 -0700

    Update redirect URL in fetch handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export default {
 	async fetch(request): Promise<Response> {
-		const destinationURL = "https://example.com";
+		const destinationURL = "https://TheFermentFarm.com/moonstone-ferments";
 		const statusCode = 301;
 		return Response.redirect(destinationURL, statusCode);
 	},
